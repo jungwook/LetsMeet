c0d9b911b91fc681fa59cbd77b5ace5f80a2230d
한정욱 <jungwook.hahn@gmail.com>
2016-05-12 22:15:30 +0900

    Initial Commit

---
+++ LetsMeet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LetsMeet
+//
+//  Created by 한정욱 on 2016. 5. 12..
+//  Copyright © 2016년 SMARTLY CO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LetsMeet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LetsMeet
+//
+//  Created by 한정욱 on 2016. 5. 12..
+//  Copyright © 2016년 SMARTLY CO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
